3c9aca6b809a9294de84f1e451f8e7369e8fc810
Mauricio Corredor <malec@uniandes.edu.co>
2022-07-17 07:14:19 -0500

    Cambios MQTT 2

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "34.201.252.21"]
+ALLOWED_HOSTS = ["localhost", "52.87.182.63"]
 
 
 # Application definition
@@ -95,8 +95,8 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "44.201.210.49",  # Dirección IP de la base de datos
-        "PORT": "",  # Puerto de la base de datos
+        "HOST": "34.239.117.176",  # Dirección IP de la base de datos
+        "PORT": "5432",  # Puerto de la base de datos
     }
 }
 
@@ -155,7 +155,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "54.166.222.111"
+MQTT_HOST = "44.203.41.235"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
