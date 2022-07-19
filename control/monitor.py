87059960a53d7ee37c6d3dae1dd94d977de4ec2e
Mauricio Corredor <malec@uniandes.edu.co>
2022-07-18 23:02:51 -0500

    Cambios Reto_S6_13

--- control/monitor.py
+++ control/monitor.py
@@ -96,8 +96,7 @@ def analyze_data():
     hum = 0
     for item in aggregation:
         alert = False
-        #variable = item["measurement__name"]
-        variable = "hidratar"
+        variable = item["measurement__name"]
         country = item['station__location__country__name']
         state = item['station__location__state__name']
         city = item['station__location__city__name']
