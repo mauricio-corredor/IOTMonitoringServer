7f49137a219f25a03ef4626421b4dfdfb72ac16e
Mauricio Corredor <malec@uniandes.edu.co>
2022-07-18 23:44:23 -0500

    Cambios Reto_S6_16

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "44.202.28.82"]
+ALLOWED_HOSTS = ["localhost", "3.89.21.142"]
 
 
 # Application definition
